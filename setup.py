ffff72068b8523abdea1ff834f5c7d931a039275
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-13 11:00:33 +0300

    Update nester() to handle indentation for nested lists. Update module version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     # Set up function's arg names
     name        = 'nester', 
-    version     = '1.0.0',
+    version     = '1.1.0',
     py_modules  = ['nester'], # Associate module's metadata with setup func's args
     author      = 'Arthur Ngondo',
     author_email = 'ngondoarthur@gmail.com',
