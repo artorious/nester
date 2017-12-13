5e00429b1d9b0b2ac8d0c0c90ac9c86a94127cd3
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-13 13:44:36 +0300

    update nester() to control indentation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     # Set up function's arg names
     name='nester',
-    version='1.2.0',
+    version='1.3.0',
     py_modules=['nester'], # Associate module's metadata w/setup func's args
     author='Arthur Ngondo',
     author_email='ngondoarthur@gmail.com',
