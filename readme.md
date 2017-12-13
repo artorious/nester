ebc86156684a67cff2f77f9bb5d2f067072bb3e5
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-13 10:03:32 +0300

    Prepare distribution - setup.py

--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 # nester - Module displays contents of a list. 
 A simple list viewer for nested items
+
+Attribution
+===========
+“Head First Python”, Paul Barry 2011.
+More Info:
+    [url](http:www.headfirstlabs.com)
+    [email](hfpython@headfirstlabs.com)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester',
+    version     = '1.0.0',
+    py_modules  = ['nester'],
+    author      = 'Arthur Ngondo',
+    author_email = 'ngondoarthur@gmail.com',
+    description = 'A simple printer of nested lists'  
+
+)
