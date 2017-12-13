4560650cb7cdc5579e1157a4c7626b93c03b19ee
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-13 10:12:29 +0300

    Build a distribution file

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+# Metadata about dist
+
 setup(
-    name        = 'nester',
+    # Set up function's arg names
+    name        = 'nester', 
     version     = '1.0.0',
-    py_modules  = ['nester'],
+    py_modules  = ['nester'], # Associate module's metadata with setup func's args
     author      = 'Arthur Ngondo',
     author_email = 'ngondoarthur@gmail.com',
     description = 'A simple printer of nested lists'  
